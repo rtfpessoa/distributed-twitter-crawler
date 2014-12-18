2dc98dccfe66d0a5a586a04e5ce9513cbc91c609
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-12-18 11:54:53 +0000

    fix json parsing

--- app/worker/Crawler.scala
+++ app/worker/Crawler.scala
@@ -4,6 +4,7 @@ import models.{UserTable, Work, WorkTable, WorkType}
 import org.joda.time.DateTime
 import play.api.Play.current
 import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads.StringReads
 import play.api.libs.json._
 import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
 import play.api.libs.ws.WS
@@ -57,30 +58,37 @@ object Crawler {
     val response = WS.url(s"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=$username&count=$count")
       .sign(OAuthCalculator(consumerKey, requestToken)).get()
 
-    val json = Await.result(response, Duration(1, MINUTES)).json
-
-    val result = json.asOpt[List[Tweet]]
-    println(result)
-    result
+    Await.result(response, Duration(1, MINUTES)).json.asOpt[List[Tweet]]
   }
 
 }
 
+case class Location(place: String, placeType: String, country: String)
 
-case class Tweet(tweetId: Long, created_at: DateTime, text: String,
-                 //url: Option[Seq[String]], mentions: Option[Seq[String]],
-                 coordinates: Option[String])
+case class Tweet(tweetId: Long, created_at: DateTime, text: String, urls: Seq[String],
+                 mentions: Seq[String], hashtags: Seq[String], location: Option[Location])
 
 object Tweet {
   val dateFormat = "E MMM d HH:mm:ss Z y"
   implicit val jodaDateTimeReads = Reads.jodaDateReads(dateFormat)
 
-  implicit val locationReads: Reads[Tweet] = (
+  val urlReads = Reads.seq((JsPath \ "expanded_url").read[String])
+  val mentionReads = Reads.seq((JsPath \ "screen_name").read[String])
+  val hashtagReads = Reads.seq((JsPath \ "text").read[String])
+
+  implicit val locationReads: Reads[Location] = (
+    (JsPath \ "name").read[String] and
+      (JsPath \ "place_type").read[String] and
+      (JsPath \ "country").read[String]
+    )(Location.apply _)
+
+  implicit val tweetReads: Reads[Tweet] = (
     (JsPath \ "id").read[Long] and
       (JsPath \ "created_at").read[DateTime] and
       (JsPath \ "text").read[String] and
-      //(JsPath \ "entities" \\ "urls" \\ "url").readNullable[Seq[String]] and
-      //(JsPath \ "entities" \\ "user_mentions" \\ "screen_name").readNullable[Seq[String]] and
-      (JsPath \ "place" \ "url").readNullable[String]
+      (JsPath \ "entities" \ "urls").read(urlReads) and
+      (JsPath \ "entities" \ "user_mentions").read(mentionReads) and
+      (JsPath \ "entities" \ "hashtags").read(hashtagReads) and
+      (JsPath \ "place").readNullable[Location]
     )(Tweet.apply _)
 }
