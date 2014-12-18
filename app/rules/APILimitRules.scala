83955a6b8e82bd0a9c21a272e644d58e953623ab
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-12-18 15:25:50 +0000

    fix limits

--- app/rules/APILimitRules.scala
+++ app/rules/APILimitRules.scala
@@ -9,24 +9,25 @@ object APILimitRules {
 
   private lazy val config = Play.configuration
   private lazy val defaultWindowDuration = Duration.standardMinutes(15)
+  private val defaultWindow = APILimit(-1, "", DateTime.now().minusDays(1), 0)
 
   def withAPILimit[A](endpoint: String)(block: => Option[A]): Option[A] = {
     val maxRequests = config.getInt(s"dtc.limit.$endpoint").getOrElse(0)
 
-    APILimitTable.getLatestWindow(endpoint).flatMap {
-      currentWindow =>
-        val window = if (currentWindow.windowStart.plus(defaultWindowDuration).isAfterNow) {
-          currentWindow
-        } else {
-          APILimitTable.create(APILimit(-1, endpoint, DateTime.now, 0))
-        }
+    val currentWindow = APILimitTable.getLatestWindow(endpoint).getOrElse(defaultWindow)
 
-        if (window.requests < maxRequests) {
-          Some(block)
-        } else {
-          None
-        }
-    }.flatten
+    val window = if (currentWindow.windowStart.plus(defaultWindowDuration).isAfterNow) {
+      currentWindow
+    } else {
+      APILimitTable.create(APILimit(-1, endpoint, DateTime.now, 0))
+    }
+
+    if (window.requests < maxRequests) {
+      block
+    } else {
+      None
+    }
+
   }
 
 }
